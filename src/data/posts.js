9375d438fd17ffd7661cded2fe8a427587b7088b
LisssTzz <deniskpiska22@mail.ru>
2026-05-27 01:17:40 +0300

    update

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,8 +1,8 @@
 const posts = [
   {
     id: 1,
-    team: "BAKS Esports",
-    logo: "/logos/baks.png",
+    team: "Esea Tracker",
+    logo: "/logos/logo.png",
     image: "/posts/baks.png",
     text: "Взлет команды Baks в рейтинге Esea Tracker, они поднимаются на 25 позиций в рейтинге получив 64 очка за выход на HLTV",
   },
