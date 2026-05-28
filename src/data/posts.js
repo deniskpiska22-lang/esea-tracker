a6da14c48e71c29a87fa37f790ef095507126b6c
LisssTzz <deniskpiska22@mail.ru>
2026-05-29 01:45:55 +0300

    update

--- src/data/posts.js
+++ src/data/posts.js
@@ -6,6 +6,13 @@ const posts = [
     image: "/posts/baks.png",
     text: "Взлет команды Baks в рейтинге Esea Tracker, они поднимаются на 25 позиций в рейтинге получив 64 очка за выход на HLTV",
   },
+    {
+    id: 1,
+    team: "Esea Tracker",
+    logo: "/logos/logo.png",
+    image: "/posts/team.png",
+    text: "Если вы не нашли себя в рейтинге, нажмите кнопку Submit team и подайте заявку, в течении этого же дня вы сможете найти себя",
+  },
 ]
 
 export default posts
